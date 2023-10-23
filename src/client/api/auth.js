15cc554aed1d59064bf2915fd80c4d269c76856d
Cartoon <sry.ongkh@gmail.com>
2023-10-24 01:55:21 +0700

    Update 24 / 10 / 66

--- src/client/api/auth.js
+++ src/client/api/auth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = "http://localhost:3000/";
 
 async function register(email, password) {
-  const response = await axios.post(API_URL + "register", {
+  const response = await axios.post(API_URL + "user/register", {
     email,
     password,
   });
